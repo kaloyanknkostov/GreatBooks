f7c1979fbd7f0d594b01a32decbe01bf67ebf4f7
kaloyan <kaloyanknkostov@gmail.com>
2026-06-29 02:09:54 +0300

    started working on back again

--- src/main/java/com/example/bookstore/user/model/User.java
+++ src/main/java/com/example/bookstore/user/model/User.java
@@ -1,11 +1,11 @@
 package com.example.bookstore.user.model;
 
 public class User {
+
     private int id;
     private String username;
     private String email;
     private int numOfRatings;
     private int avgRatings;
     private String description;
-    private
 }
